e9bdc415d02998891ed2c6f34571444c42aff158
Ryconler <1290279000@qq.com>
2018-12-20 15:34:33 +0800

    v1.1

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var gamesRouter=require("./routes/games")
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'public/views'));   //设置视图的目录
+app.set('views', path.join(__dirname, 'public'));   //设置视图的目录
 app.engine('html', require('ejs').renderFile);  //使用ejs引擎渲染html文件
 app.set('view engine', 'html');
 
---
+++ public/javascripts/indexScript.js
@@ -0,0 +1,15 @@
+// $(function () {
+//     $("#game").on("mouseover",function () {
+//         $(".menu1").css("display","block")
+//     })
+//
+//     $(".menu1").on("mouseover",function () {
+//         $(".menu1").css("display","block")
+//     })
+//     $("#game").on("mouseleave",function () {
+//         $(".menu1").css("display","none")
+//     })
+//     $(".menu1").on("mouseleave",function () {
+//         $(".menu1").css("display","none")
+//     })
+// })
