e4445cbb14153954fbfebd1b8f300513c8733c01
Ryconler <1290279000@qq.com>
2019-01-16 22:51:20 +0800

    v2.1.4：聊天室更新

--- public/javascripts/chatroom.js
+++ public/javascripts/chatroom.js
@@ -2,47 +2,47 @@ $(function () {
 
     var socket = io.connect('http://111.231.200.245:3000');
     // var socket = io.connect('http://localhost:3000');
-    //点击发送数据
+
+    //按钮发送数据
     $(".input button").click(function () {
-        var text = $(".input input").val().trim()
-        if(text===""){
-            alert("输入不能为空")
-        }else {
-            $(".input input").val("")
-            var d = new Date()
-            var date =d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds()
-            socket.emit('send', {content: text, date: date});
-        }
+        emitData()
     })
     //回车发送数据
     $(".input input").keypress(function (e) {
-        if(e.key==="Enter"){
-            var text = $(this).val()
-            if(text===""){
-                alert("输入不能为空")
-            }else {
-                $(".input input").val("")
-                var d = new Date()
-                var date =  d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds()
-                socket.emit('send', {content: text, date: date});
-            }
+        if (e.key === "Enter") {
+            emitData()
         }
     })
+
+    //发送数据
+    function emitData() {
+        var text = $(".input input").val().trim()
+        if (text === "") {
+            alert("内容不能为空")
+        } else {
+            $(".input input").val("")
+            var d = new Date()
+            var date = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds()
+            socket.emit('send', {content: text, date: date});
+        }
+    }
+
     //更新数据
     function updateData(data) {
         $(".content .wrap").append("<div class='record'><p>"
-            +data.content +"</p><p>"
-            +data.date +"</p></div> ")
+            + data.content + "</p><p>"
+            + data.date + "</p></div> ")
         //让内容滚动条始终最下方
         $(".content .wrap").scrollTop($(".content .wrap")[0].scrollHeight)
     }
+
     //接收新消息
     socket.on('new', function (data) {
         updateData(data)
     });
     //接收在线人数信息
-    socket.on('connectCount',function(count){
-        $("h1").text("当前在线人数："+count)
+    socket.on('connectCount', function (count) {
+        $("h1").text("当前在线人数：" + count)
     });
 
 })
