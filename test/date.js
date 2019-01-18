89595e92676dd135782c4a9ed4a04193b4bba6f6
Ryconler <1290279000@qq.com>
2019-01-18 20:44:28 +0800

    v2.2.5：文件调整

--- test/date.js
+++ test/date.js
@@ -1,3 +1,2 @@
-
 var d=new Date()
 console.log(d.getFullYear()+"-"+(d.getMonth()+1)+"-"+d.getDate()+" "+d.getHours()+":"+d.getMinutes()+":"+d.getSeconds())
--- test/mindex.js
+++
@@ -1,8 +0,0 @@
-function calc() {
-    var html=document.documentElement
-    width=html.clientWidth>768?768:html.clientWidth
-    html.style.fontSize=30*(width/384)+"px"
-    // console.log(html.style.fontSize)
-}
-window.addEventListener("resize",calc)
-calc()
