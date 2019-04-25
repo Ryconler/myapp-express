f1ca936e14c870b85fac51210106392ae4119c71
Ryconler <1290279000@qq.com>
2019-04-25 10:43:38 +0800

    .

--- routes/MysqlConnect.js
+++ routes/MysqlConnect.js
@@ -3,7 +3,7 @@ function handleError () {
     var conn = mysql.createConnection({
         host: 'localhost',
         user: 'root',
-        password: 'ntu2015',
+        password: 'root',
         database: 'myapp',
         port: 3306,
         // useConnectionPooling: true
